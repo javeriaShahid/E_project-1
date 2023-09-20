99d76b32e51881bd5c5f4c2fe32b28f1c4d59ea5
AJ2235 <ariba2209f@aptechgdn.net>
2023-09-20 20:17:54 +0500

    commit

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -19,15 +19,15 @@
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="single-event-area mb-30">
                         <div class="event-thumbnail">
-                            <img src="img/bg-img/e1.jpg" alt="">
+                            <a href="/atif">
+                                 <img src="img/artist/Atif 3.jpg" alt="">
+                            </a>
+
                         </div>
                         <div class="event-text">
-                            <h4>Dj Night Party</h4>
-                            <div class="event-meta-data">
-                                <a href="#" class="event-place">VIP Sala</a>
-                                <a href="#" class="event-date">June 15, 2018</a>
-                            </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <h4>Atif Aslam</h4>
+                         
+                            <a href="/atif" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -36,15 +36,14 @@
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="single-event-area mb-30">
                         <div class="event-thumbnail">
-                            <img src="img/bg-img/e2.jpg" alt="">
+                            <a href="/asim">
+                            <img src="img/artist/asimazhar_1.webp" alt="">
+                            </a>
                         </div>
                         <div class="event-text">
-                            <h4>The Mission</h4>
-                            <div class="event-meta-data">
-                                <a href="#" class="event-place">Gold Arena</a>
-                                <a href="#" class="event-date">June 15, 2018</a>
-                            </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <h4>Asim Azhar</h4>
+                           
+                            <a href="/asim" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -52,16 +51,14 @@
                 <!-- Single Event Area -->
                 <div class="col-12 col-md-6 col-lg-4">
                     <div class="single-event-area mb-30">
-                        <div class="event-thumbnail">
-                            <img src="img/bg-img/e3.jpg" alt="">
+                        <div class="event-thumbnail"><a href="/ali">
+                            <img src="img/artist/ali zafar 9.jpg" alt="">
+                            </a>
                         </div>
                         <div class="event-text">
-                            <h4>Planet ibiza</h4>
-                            <div class="event-meta-data">
-                                <a href="#" class="event-place">Space Ibiza</a>
-                                <a href="#" class="event-date">June 15, 2018</a>
-                            </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <h4>Ali Zafar</h4>
+                            
+                            <a href="/ali" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -73,12 +70,12 @@
                             <img src="img/bg-img/e4.jpg" alt="">
                         </div>
                         <div class="event-text">
-                            <h4>Dj Night Party</h4>
+                            <h4>Rahat Fateh Ali Khan</h4>
                             <div class="event-meta-data">
                                 <a href="#" class="event-place">VIP Sala</a>
                                 <a href="#" class="event-date">June 15, 2018</a>
                             </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <a href="#" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -90,12 +87,12 @@
                             <img src="img/bg-img/e5.jpg" alt="">
                         </div>
                         <div class="event-text">
-                            <h4>The Mission</h4>
+                            <h4>Falak Shabbir</h4>
                             <div class="event-meta-data">
                                 <a href="#" class="event-place">Gold Arena</a>
                                 <a href="#" class="event-date">June 15, 2018</a>
                             </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <a href="#" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -107,12 +104,12 @@
                             <img src="img/bg-img/e6.jpg" alt="">
                         </div>
                         <div class="event-text">
-                            <h4>Planet ibiza</h4>
+                            <h4>Farhan Saeed</h4>
                             <div class="event-meta-data">
                                 <a href="#" class="event-place">Space Ibiza</a>
                                 <a href="#" class="event-date">June 15, 2018</a>
                             </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <a href="#" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -124,12 +121,12 @@
                             <img src="img/bg-img/e7.jpg" alt="">
                         </div>
                         <div class="event-text">
-                            <h4>Dj Night Party</h4>
+                            <h4>BIlal Saeed</h4>
                             <div class="event-meta-data">
                                 <a href="#" class="event-place">VIP Sala</a>
                                 <a href="#" class="event-date">June 15, 2018</a>
                             </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <a href="#" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -141,12 +138,12 @@
                             <img src="img/bg-img/e8.jpg" alt="">
                         </div>
                         <div class="event-text">
-                            <h4>The Mission</h4>
+                            <h4>Young Stunners</h4>
                             <div class="event-meta-data">
                                 <a href="#" class="event-place">Gold Arena</a>
                                 <a href="#" class="event-date">June 15, 2018</a>
                             </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <a href="#" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
@@ -158,25 +155,19 @@
                             <img src="img/bg-img/e9.jpg" alt="">
                         </div>
                         <div class="event-text">
-                            <h4>Planet ibiza</h4>
+                            <h4>Hadiqa Kiani</h4>
                             <div class="event-meta-data">
                                 <a href="#" class="event-place">Space Ibiza</a>
                                 <a href="#" class="event-date">June 15, 2018</a>
                             </div>
-                            <a href="#" class="btn see-more-btn">See Event</a>
+                            <a href="#" class="btn see-more-btn">Songs</a>
                         </div>
                     </div>
                 </div>
 
             </div>
 
-            <div class="row">
-                <div class="col-12">
-                    <div class="load-more-btn text-center mt-70">
-                        <a href="#" class="btn oneMusic-btn">Load More <i class="fa fa-angle-double-right"></i></a>
-                    </div>
-                </div>
-            </div>
+           
         </div>
     </section>
     <!-- ##### Events Area End ##### -->
