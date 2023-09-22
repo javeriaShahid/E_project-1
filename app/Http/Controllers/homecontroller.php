65375e881b5b8a199c432aad421a9ec80ddc2a8b
AJ2235 <ariba2209f@aptechgdn.net>
2023-09-22 20:43:47 +0500

    commit

--- app/Http/Controllers/homecontroller.php
+++ app/Http/Controllers/homecontroller.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
-
+use illuminate\Support\Facades\Auth;
 class homecontroller extends Controller
 {
  public function redirect(){
+    $usertype=Auth::user()->usertype;
 if($usertype === '1'){
     return view('admin.home');
 }
