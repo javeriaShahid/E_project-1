092335caabd3693b681dcc70c6dbc9d6390096c1
anusha-amin17 <123258746+anusha-amin17@users.noreply.github.com>
2023-09-27 20:31:39 +0500

    Update justin.blade.php

--- resources/views/justin.blade.php
+++ resources/views/justin.blade.php
@@ -1,1 +1,158 @@
+@extends("components.header")
+@section("new")
+    <!-- ##### Breadcumb Area Start ##### -->
+    <section class="breadcumb-area bg-img-3 bg-overlay">
+        <div class="bradcumbContent">
+            <p>Songs</p>
+            <h2>Justin Bieber</h2>
+        </div>
+    </section>
+    <!-- ##### Breadcumb Area End ##### -->
 
+<br><br>
+     <!-- ##### Song Area Start ##### -->
+     <div class="one-music-songs-area mb-70">
+        <div class="container">
+            <div class="row">
+
+                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 7.webp" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>Company</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 8.jpg" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>I Feel Funny</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 5.jpg" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>Friends</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+                                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 5.jpg" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>As I am</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+                                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 5.jpg" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>Somebody</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+                                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 5.jpg" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>Life Is Worth Living</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+                                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 5.jpg" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>Honest</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Single Song Area -->
+                <div class="col-12">
+                    <div class="single-song-area mb-30 d-flex flex-wrap align-items-end">
+                        <div class="song-thumbnail">
+                            <img src="img/artist/Asim Azhar 5.webp" alt="">
+                        </div>
+                        <div class="song-play-area">
+                            <div class="song-name">
+                                <p>Unstable</p>
+                            </div>
+                            <audio preload="auto" controls>
+                                <source src="">
+                            </audio>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- ##### Song Area End ##### -->
+    @endsection
